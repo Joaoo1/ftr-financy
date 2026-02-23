a8ccdcaac8bca0e7b6eca6a4aac5e84cd0f53efb
João <silva.joao1@outlook.com.br>
2026-02-23 00:41:59 -0300

    feat(frontend): initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import logo from "./assets/logo.svg";
+
+export function App() {
+  return (
+    <div>
+      <h1>Financy</h1>
+      <img src={logo} alt="Logo Financy" className="w-32 h-8" />
+    </div>
+  );
+}
