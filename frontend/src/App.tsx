11058753c622a543c189fecb1fcb085e1ebf19dc
João <silva.joao1@outlook.com.br>
2026-02-23 12:27:29 -0300

    feat(frontend): setup react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,5 @@
-import logo from "./assets/logo.svg";
+import { Router } from "./routes";
 
 export function App() {
-  return (
-    <div>
-      <h1>Financy</h1>
-      <img src={logo} alt="Logo Financy" className="w-32 h-8" />
-    </div>
-  );
+  return <Router />;
 }
